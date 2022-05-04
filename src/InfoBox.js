529a84ec02e3e7a0dec4c013233d431ba739df21
Patryk Ryczek <patrykryczek00@gmail.com>
2022-05-04 16:49:30 +0200

    first commit

---
+++ src/InfoBox.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Card, CardContent, Typography } from "@material-ui/core" 
+
+function InfoBox({ title, cases, total }) {
+  return (
+    <Card className="infoBox">
+        <CardContent>
+            <Typography color="textSecondary" className="infobox__title">
+                {title}
+            </Typography>
+            <h2 className="infoBox__cases">{cases}</h2>
+            <Typography className="infobox__total">
+                {total} Total
+            </Typography>
+          
+
+        </CardContent>
+    </Card>
+  )
+}
+
+export default InfoBox
